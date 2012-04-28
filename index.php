fbbbc70fe42949d7bfad98cccce19b82ea4891b2
unknown <Administrator@cbjs-acer.(none)>
2012-04-28 21:12:26 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="utf-8"/>
+	<title></title>
+	<link rel="stylesheet" href="css/main.css" type="text/css"/>
+    <head>
+    
+    <body>
+看	this is a template file.
+    </body>
+</html>
